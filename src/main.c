4b409d0bc9586956f2bc81723a2740ba1b7127d2
John Howe <johnhowe@gmail.com>
2010-07-22 00:45:12 +1200

    this is a mess

--- src/main.c
+++ src/main.c
@@ -5,33 +5,12 @@
  *      John Howe	2010
  */
 
-#include "config.h"
-#include "init.h"
-#include "lcd.h"
-#include "timers.h"
-#include "HG24016001G.h"
+//#define AT91SAM7S256
+#include "../include/Board.h"
 
-void setTestLed(int state) {
-    if (state == on)
-        AT91F_PIO_SetOutput(AT91C_BASE_PIOA, LED_A);
-    if (state == off)
-        AT91F_PIO_ClearOutput(AT91C_BASE_PIOA, LED_A);
-}
 
 int main(void) {
-    initController();
-    initPIO();
-    initTimers();
-    initLCD();
-
-    testDisplay ();
-
     for (;;) {
-        busyWait(5000);
-        setTestLed(on);
-        busyWait(5000);
-        setTestLed(off);
-        nothing();
     }
     return (0);
 }
