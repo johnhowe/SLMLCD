bdcc2d070f0ee00a4ad1df6a0101ae70b5850375
John Howe <johnhowe@gmail.com>
2010-07-28 14:07:49 +1200

    flash is a bit flakey. seems to need uC unplugging before it will work

--- src/flash.sh
+++ src/flash.sh
@@ -3,17 +3,21 @@ if [ `pidof openocd` ] ; then killall openocd ; fi
 
 openocd -f ../tools/openocd/openocd.cfg & 
 
-sleep 0.5
+sleep 2
+
 ( 
-echo halt
-sleep 0.5
-echo flash write_bank 0 main.bin 0x0
-sleep 0.5
-echo reset
-sleep 0.5
-echo shutdown
+    echo halt
+    sleep 1
+    echo flash write_bank 0 main.bin 0x0
+    sleep 1
+    echo reset
+    sleep 1
+    echo shutdown
 ) | telnet localhost 4444
 
 sleep 1
 if [ `pidof openocd` ] ; then killall openocd ; fi
+
+
+
 
