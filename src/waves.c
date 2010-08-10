8ac4c8720144990ee10bcd4ce7cd3daf6814da69
John Howe <johnhowe@gmail.com>
2010-08-10 13:39:00 +1200

    separated waves func to its own file

---
+++ src/waves.c
@@ -0,0 +1,62 @@
+/*
+ * waves.c
+ *
+ *  Spatial Light Modulator
+ *      John Howe	2010
+ */
+
+void animateWaves (void)
+{
+    uint16 front = 0;
+    for(;;)
+    {
+        drawWaves (WAVELENGTH, front);
+        front++;
+        if (front == WAVELENGTH)
+        {
+            front = 0;
+        }
+    }
+}
+
+/* Write unstructured data to LCD */
+void drawWaves (uint8 wavelength, uint8 wavefront)
+{
+    colour_t colour;
+    colour.shade = WHITE>>3; // care as colours <<3'd
+    colour.direction = RISING;
+
+    for (int i = 0; i < wavefront; i++)
+        shiftFront (&colour);
+
+    uint16 pix = prepDisplay(0, 0, 240, 160);
+    while (pix--)
+    {
+        write (DATA, colour.shade<<3);
+        write (DATA, colour.shade<<3);
+        write (DATA, colour.shade<<3);
+
+        if (!(pix % 62)) // shifts colour at each row
+        {
+            shiftFront (&colour);
+            shiftFront (&colour);
+        }
+    }
+}
+
+/* Moves the colour according to its direction */
+void shiftFront (colour_t *colour)
+{
+    if (colour->direction == RISING) // white -> black
+    {
+        colour->shade++;
+        if (colour->shade == (BLACK >> 3))
+            colour->direction = FALLING;
+    }
+    else // black -> white
+    {
+        colour->shade--;
+        if (colour->shade == (WHITE >> 3))
+            colour->direction = RISING;
+    }
+}
